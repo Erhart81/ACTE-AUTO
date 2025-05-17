67eed1cf89a882df3668a67aca947b127b581101
Erhart Darian <lucaerhartdarian@gmail.com>
2025-05-17 10:49:51 +0300

    Primul commit - frontend vite

---
+++ src/ContractAutoDemo.jsx
@@ -0,0 +1,8 @@
+export default function ContractAutoDemo() {
+  return (
+    <div>
+      <h1>Contract Auto NonStop</h1>
+      <p>Încarcă CI, Talon și CIV pentru a genera contractul PDF.</p>
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ContractAutoDemo from "./ContractAutoDemo.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ContractAutoDemo />
+  </React.StrictMode>
+);
