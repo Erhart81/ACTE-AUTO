537fc87ab1338d5f42736447d1a228fc8d06b6e6
Erhart81 <asigurarisimeria@yahoo.com>
2025-05-20 14:00:22 +0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Contract Auto Nonstop</h1>
+      <p>Aplicație pentru generarea de contracte auto.</p>
+    </div>
+  );
+}
+
+export default App;
